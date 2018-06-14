9202882a6f32745a0c89fe0d5f8945e139011a62
danny <dtylman@gmail.com>
2018-06-14 10:44:54 +0300

    fixing ExecJS test

--- app_test.go
+++ app_test.go
@@ -103,7 +103,9 @@ func TestExecJS(t *testing.T) {
 	var buf bytes.Buffer
 	Output = io.Writer(&buf)
 	ExecJS("alert('this  is all the testing possible without running NWJS, hope itl do')")
-	render(NewElement("hello world"), Output)
-	assert.Equal(t, "<hello world id=\"_hello world1\"></hello world>\n$alert('this  is all the testing possible without running NWJS, hope itl do')\n", buf.String())
+	elem := NewElement("hello world")
+	elem.SetID("_hello world1")
+	render(elem, Output)
+	assert.Equal(t, `<hello world id="_hello world1"></hello world>`+"\n"+`$alert('this  is all the testing possible without running NWJS, hope itl do')`+"\n", buf.String())
 	Output = os.Stdout
 }
